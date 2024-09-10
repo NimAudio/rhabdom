eed6f6a1a571932db0a00e36dfc630b73c46bdc1
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2024-09-10 16:56:19 -0400

    remove separate location map i had previously put in inputs

--- resource.nim
+++ resource.nim
@@ -1,4 +1,4 @@
-import std/tables
+import std/[tables, strutils]
 import pugl
 
 type
@@ -49,7 +49,7 @@ type
         update_cb     *: proc (globals: pointer, data: ptr UncheckedArray[byte]): int = nil # can return new update frequency
         update_frames *: int # 0 does not run update_cb, 1 is every frame, 2 is every other frame
 
-    MeshData* = object
+    MeshData* = ref object
         num_vertices *: int
         num_indices  *: int
         ibo_id       *: uint32
@@ -85,13 +85,13 @@ proc byte_size*(mat: MeshAttrType): int =
         of matF16:
             result = 2
 
-proc mesh_setup*(md: var MeshData, locations: Table[string, uint32]) =
+proc mesh_setup*(md: var MeshData) =
     glGenVertexArrays(1, addr md.vao_id)
     glBindVertexArray(md.vao_id)
 
     glGenBuffers(1, addr md.ibo_id)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, md.ibo_id)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, GLsizeiptr(md.num_indices * uint32.sizeof), addr md.indices, GLenum(md.index_usage))
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, GLsizeiptr(md.num_indices * uint32.sizeof), md.indices, GLenum(md.index_usage))
 
     for buf in md.buffers:
         glGenBuffers(1, addr buf.vbo_id)
@@ -99,13 +99,20 @@ proc mesh_setup*(md: var MeshData, locations: Table[string, uint32]) =
         var vert_size = 0
         for attr in buf.attributes:
             vert_size += attr.number * byte_size(attr.var_type)
+        # var floats = cast[ptr UncheckedArray[float32]](buf.data)
+        # for i in 0..<28:
+        #     stdout.write(floats[i])
+        #     stdout.write(" ")
+        # stdout.write("\n")
         glBufferData(GL_ARRAY_BUFFER, GLsizeiptr(vert_size * md.num_vertices), buf.data, GLenum(buf.usage))
 
         if buf.interlaced:
             var attr_byte_pos = 0
+            echo(len(buf.attributes))
             for attr in buf.attributes:
+                echo(attr.location, " ", attr.number, " ", attr.var_type, " ", attr.normalize, " ", vert_size, " ", attr_byte_pos)
                 glVertexAttribPointer(
-                    GLuint(locations[attr.name]), # id/location
+                    GLuint(attr.location), # id/location
                     GLint(attr.number), # number of components
                     GLenum(attr.var_type), # type of attr
                     GLboolean(attr.normalize),
@@ -113,12 +120,14 @@ proc mesh_setup*(md: var MeshData, locations: Table[string, uint32]) =
                     cast[pointer](attr_byte_pos) # offset based on total size of previous attrs
                 )
                 attr_byte_pos += attr.number * byte_size(attr.var_type) # size of one vertex worth of the attr
-                glEnableVertexAttribArray(GLuint(locations[attr.name]))
+                glEnableVertexAttribArray(GLuint(attr.location))
         else:
             var attr_byte_pos = 0
+            echo(len(buf.attributes))
             for attr in buf.attributes:
+                echo(attr.location, " ", attr.number, " ", attr.var_type, " ", attr.normalize, " ", attr.number * byte_size(attr.var_type), " ", attr_byte_pos)
                 glVertexAttribPointer(
-                    GLuint(locations[attr.name]), # id/location
+                    GLuint(attr.location), # id/location
                     GLint(attr.number), # number of components
                     GLenum(attr.var_type), # type of attr
                     GLboolean(attr.normalize),
@@ -126,21 +135,22 @@ proc mesh_setup*(md: var MeshData, locations: Table[string, uint32]) =
                     cast[pointer](attr_byte_pos) # offset based on total size of all verts worth of previous attrs
                 )
                 attr_byte_pos += attr.number * byte_size(attr.var_type) * md.num_vertices # size of all vertices worth of the attr
-                glEnableVertexAttribArray(GLuint(locations[attr.name]))
+                glEnableVertexAttribArray(GLuint(attr.location))
 
     glBindBuffer(GL_ARRAY_BUFFER, 0)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
     glBindVertexArray(0)
+    echo(md[])
 
 proc mesh_frame*(md: MeshData) =
     # add blend funcs
     # add general glenable/disable system
     glBindVertexArray(md.vao_id)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, md.ibo_id)
-    glDrawElements(GLenum(md.mesh_type), GLsizei(md.num_indices), GL_TYPE_UNSIGNED_INT, nil);
+    glDrawElements(GLenum(md.mesh_type), GLsizei(md.num_indices), GL_TYPE_UNSIGNED_INT, md.indices);
 
 proc register_mesh*(rm: var RhabdomMeshes, name: string, md: MeshData) =
-    rm.mesh_str_map[name] = len(rm.meshes)
+    rm.mesh_str_map[name.toUpperAscii()] = len(rm.meshes)
     rm.meshes &= md
 
 
